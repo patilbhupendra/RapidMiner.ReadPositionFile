e84e760e92f4f9c50adfb309b62e9e4744b0a7b5
Bhupendra Patil <bpatil@rapidminer.com>
2016-09-21 14:31:17 +0100

    supports encoding and pick up file

--- src/main/java/com/rapidminer/operator/ReadFilePositional.java
+++ src/main/java/com/rapidminer/operator/ReadFilePositional.java
@@ -1,6 +1,7 @@
 package com.rapidminer.operator;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import com.rapidminer.RapidMiner;
 import com.rapidminer.example.Attribute;
 import com.rapidminer.example.ExampleSet;
 import com.rapidminer.example.table.AttributeFactory;
@@ -15,8 +17,12 @@ import com.rapidminer.example.table.DoubleArrayDataRow;
 import com.rapidminer.example.table.MemoryExampleTable;
 import com.rapidminer.operator.io.AbstractReader;
 import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeFile;
 import com.rapidminer.parameter.ParameterTypeString;
+import com.rapidminer.parameter.ParameterTypeStringCategory;
 import com.rapidminer.tools.Ontology;
+import com.rapidminer.tools.ParameterService;
+import com.rapidminer.tools.io.Encoding;
 
 public class ReadFilePositional extends AbstractReader<ExampleSet> {
 
@@ -33,6 +39,11 @@ public class ReadFilePositional extends AbstractReader<ExampleSet> {
 		// TODO Auto-generated method stub
 		String fileName = getParameterAsString("FILE_PATH");
 		String partitions = getParameterAsString("POSITIONS");
+
+		String charsetName = getParameterAsString("PARAMETER_ENCODING");
+
+		Charset charSet = Encoding.getEncoding(charsetName);
+
 		List<String> positions = Arrays.asList(partitions.split(","));
 		int_positions = new int[positions.size()];
 		// position.forEach(arg0);
@@ -43,7 +54,7 @@ public class ReadFilePositional extends AbstractReader<ExampleSet> {
 
 		table = createTable();
 
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+		try (Stream<String> stream = Files.lines(Paths.get(fileName), charSet)) {
 
 			stream.forEach(currentline -> splitandAdd(currentline));
 
@@ -72,7 +83,7 @@ public class ReadFilePositional extends AbstractReader<ExampleSet> {
 	}
 
 	private void splitandAdd(String currentline) {
-		// attributes.get(attcounter).getMapping().mapString(field.getValue().toString());
+
 		int numberOfColumns = int_positions.length - 1;
 		double[] values = new double[numberOfColumns];
 
@@ -94,13 +105,24 @@ public class ReadFilePositional extends AbstractReader<ExampleSet> {
 	public List<ParameterType> getParameterTypes() {
 		List<ParameterType> types = super.getParameterTypes();
 
-		ParameterTypeString filepath = new ParameterTypeString("FILE_PATH",
-				"Enter the PATH OF FILE");
+		ParameterTypeString filepath = new ParameterTypeFile("FILE_PATH",
+				"Enter the PATH OF FILE", "", false);
 		types.add(filepath);
 
 		ParameterTypeString positions = new ParameterTypeString("POSITIONS",
 				"Enter the positions");
 		types.add(positions);
+		final String[] CHARSETS = Encoding.CHARSETS;
+
+		String encoding = RapidMiner.SYSTEM_ENCODING_NAME;
+		String encodingProperty = ParameterService
+				.getParameterValue(RapidMiner.PROPERTY_RAPIDMINER_GENERAL_DEFAULT_ENCODING);
+		if (encodingProperty != null) {
+			encoding = encodingProperty;
+		}
+		types.add(new ParameterTypeStringCategory("PARAMETER_ENCODING",
+				"The encoding used for reading or writing files.", CHARSETS,
+				encoding, false));
 
 		return types;
 
